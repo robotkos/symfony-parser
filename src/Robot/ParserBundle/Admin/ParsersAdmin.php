251e55f266cf9caa99b5022501cda8ef71ef3917
stas <stas.sh23@gmail.com>
2015-09-26 12:05:20 +0300

    Добавил Парсер

---
+++ src/Robot/ParserBundle/Admin/ParsersAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Robot\ParserBundle\Admin;
+ 
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+ 
+class ParsersAdmin extends Admin
+{
+    public function getBatchActions()
+    {
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id', null, array('label' => 'Id'))
+                   ->add('pn', null, array('label' => 'pn'))
+                   ->add('oldPn', null, array('label' => 'oldPn'))
+                   ->add('descr', null, array('label' => 'descr'))
+                   ->add('img', null, array('label' => 'img'));
+    }
+ 
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('pn')
+                   ->add('oldPn')
+                   ->add('descr')
+                   ->add('img');
+    }
+
+  protected function configureRoutes(RouteCollection $collection)
+  {
+      #$collection->add('parsers', $this->getRouterIdParameter().'/parsers');
+  }
+
+
+}
